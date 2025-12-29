f913f62c334d87e55637213e129bf009ba0d7bda
Mahdi <mahdi@email.comakideakide67@gmail.com>
2025-12-29 12:02:41 -0800

    fix db env variable name for Render

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
   ssl: false
 });
