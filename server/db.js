2d92e9076e3224b268e431219030b5454ff94225
Mahdi <mahdi@email.comakideakide67@gmail.com>
2025-12-29 10:45:13 -0800

    fix db env variable name for Render

--- server/db.js
+++ server/db.js
@@ -15,7 +15,7 @@ db.connect((err) => {
     console.error("Connection config:", {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
-      database: process.env.MYSQLDATABASE,
+      database: process.env.DB_NAME,
       port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
       hasPassword: !!process.env.DB_PASSWORD
     });
